c9771293d6923b8be4583e5884f1b0110e39a70a
Aabiskar Pandey <pandeyaabiskar@gmail.com>
2023-01-23 13:16:25 +0545

    MUI

--- src/containers/TodoLayout/index.js
+++ src/containers/TodoLayout/index.js
@@ -1,6 +1,13 @@
 import { useState, useEffect } from "react";
-import Button from "../../components/Button";
+// import Button from "../../components/Button";
 import Input from "../../components/Input"
+
+import { Button }  from '@mui/material';
+import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
 
 function TodoLayout() {
   const [todos, setTodos] = useState([]);
@@ -28,6 +35,10 @@ function TodoLayout() {
     }
   }, [todos])
 
+  const CustomizedButton = styled(Button)(({ theme }) => ({
+    backgroundColor: theme.palette.primary.main,
+  }));
+
 
 
   const handleInput = () => {
@@ -40,23 +51,37 @@ function TodoLayout() {
   };
 
   return (
-    <>
-      <h1>Todo Application</h1>
-      <div>
-        <Input value={todoText} onChange={handleInputText} />
-        <Button onClick={handleInput}>Add</Button>
-      </div>
-      <div>
-        <ul>
+    <Grid container spacing={2}>
+      <Grid item xs={12} >
+        <h1>Todo Application</h1>
+      </Grid>
+      <Grid item xs={10}>
+              <TextField
+          id="todo"
+          label="Todo"
+          value={todoText}
+          onChange={handleInputText}
+          size="small"
+          fullWidth
+        />
+      </Grid>
+      <Grid item xs={2}>
+        <CustomizedButton variant="contained" onClick={handleInput} fullWidth           
+        >Text</CustomizedButton>
+      </Grid>
+      <Grid item xs={12}>
+               <ul>
           {todos.map((todo, index) => {
             return <li key={index}>{todo}</li>;
           })}
         </ul>
-      </div>
-      <div>
-        <p>Todo Count : {todos.length}</p>
-      </div>
-    </>
+      </Grid>
+      <Grid item xs={12}>
+                       <p>Todo Count : {todos.length}</p>
+
+        
+      </Grid>
+  </Grid>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
